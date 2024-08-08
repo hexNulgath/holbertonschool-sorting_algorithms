0c98a160d5a0ad35af564a9a2d2cf6dab516a71e
hexNulgath <machadoignacio@hotmail.com>
2024-08-08 20:44:06 +0000

    added task 1

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -6,5 +6,21 @@
  */
 void bubble_sort(int *array, size_t size)
 {
+	size_t swapped = 1, i, temp;
+	while (swapped == 1)
+	{
+		swapped = 0;
+		for (i=0; i < size - 1; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				temp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = temp;
+				swapped = 1;
+				print_array(array, size);
+			}
+		}
+	}
 	print_array(array, size);
 }

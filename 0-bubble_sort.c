50b7d2bf5d9ebc6a80e95eb2bba346e87bc0ed08
hexNulgath <machadoignacio@hotmail.com>
2024-08-08 20:28:59 +0000

    added O notations complexity time

---
+++ 0-bubble_sort.c
@@ -0,0 +1,10 @@
+#include "sort.h"
+/**
+ * bubble_sort - sorts an array of integers in ascending order using the Bubble sort algorithm
+ * @size: size of the array
+ * @array: array to order
+ */
+void bubble_sort(int *array, size_t size)
+{
+	print_array(array, size);
+}

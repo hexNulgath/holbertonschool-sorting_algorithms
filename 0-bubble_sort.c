6521eb25b9224ff8d09891f1463becfd7f5eddce
hexNulgath <machadoignacio@hotmail.com>
2024-08-08 20:49:29 +0000

    added task 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,16 +1,17 @@
 #include "sort.h"
 /**
- * bubble_sort - sorts an array of integers in ascending order using the Bubble sort algorithm
+ * bubble_sort - sorts using the Bubble sort algorithm
  * @size: size of the array
  * @array: array to order
  */
 void bubble_sort(int *array, size_t size)
 {
 	size_t swapped = 1, i, temp;
+
 	while (swapped == 1)
 	{
 		swapped = 0;
-		for (i=0; i < size - 1; i++)
+		for (i = 0; i < size - 1; i++)
 		{
 			if (array[i] > array[i + 1])
 			{

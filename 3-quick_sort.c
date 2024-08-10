24e0ed59436a30b878cfea6c1edeb239ced82cff
hexNulgath <machadoignacio@hotmail.com>
2024-08-10 03:12:53 +0000

    added task 3

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -6,40 +6,34 @@
  * @lo: lowest indes to compare
  * @size: size of the whole array
  */
-void quick_sort_rec(int *array, size_t hi, size_t lo, size_t size)
+void quick_sort_rec(int *array, size_t lo, size_t hi, size_t size)
 {
-	size_t i, pivot;
+	size_t i, j, pivot;
 	int temp;
+	if (lo >= hi)
+		return;
 
 	pivot = hi;
 	i = lo;
-	while (i < pivot)
+
+	for (j = lo; j < hi; j++)
 	{
-		if (array[pivot] < array[i])
+		if (array[j] < array[pivot])
 		{
-			temp = array[pivot - 1];
-			array[pivot - 1] = array[pivot];
-			if (pivot - 1 == i)
-				array[pivot] = temp;
-			else
-			{
-				array[pivot] = array[i];
-				array[i] = temp;
-			}
-			print_array(array, size);
-			pivot = pivot - 1;
-		}
-		else
+			temp = array[i];
+			array[i] = array[j];
+			array[j] = temp;
+			if (i != j)
+				print_array(array, size);
 			i++;
+		}
 	}
-	if (pivot > lo)
-	{
-		quick_sort_rec(array, pivot - 1, lo, size);
-	}
-	if (pivot < hi)
-	{
-		quick_sort_rec(array, hi, pivot + 1, size);
-	}
+	temp = array[i];
+	array[i] = array[pivot];
+	array[pivot] = temp;
+	if (i > 0)
+		quick_sort_rec(array, lo, i - 1 , size);
+	quick_sort_rec(array, i + 1, hi, size);
 }
 /**
  * quick_sort - order using the Quick sort algorithm
@@ -48,5 +42,7 @@ void quick_sort_rec(int *array, size_t hi, size_t lo, size_t size)
  */
 void quick_sort(int *array, size_t size)
 {
-	quick_sort_rec(array, size - 1, 0, size);
+	if (size < 2)
+		return;
+	quick_sort_rec(array, 0, size - 1, size);
 }

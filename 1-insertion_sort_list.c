1460bef463832ea06b30277abad35625fcc17751
hexNulgath <machadoignacio@hotmail.com>
2024-08-10 03:38:21 +0000

    added task 1

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,34 +1,56 @@
 #include "sort.h"
+/**
+ * listint_len - calculate length of list
+ * @h: list header
+ * Return: lenght
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t n = 0;
 
-
+	while (h != NULL)
+	{
+		h = h->next;
+		n++;
+	}
+	return (n);
+}
+/**
+ * insertion_sort_list - sorts a list
+ * @list: list header
+ */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *head = NULL;
+	listint_t *current, *next, *temp, *prev;
 
-	while (*list != NULL)
+	if (list == NULL || list == NULL)
+		return;
+
+	current = (*list)->next;
+	while (current != NULL)
 	{
-		listint_t *current = *list;
-		*list = (*list)->next;
-		if (head == NULL || current->n < head->n)
+		next = current->next;
+		temp = current;
+
+		while (temp->prev != NULL && temp->prev->n > temp->n)
 		{
-			current->next = head;
-			head = current;
+			prev = temp->prev;
+			next = temp->next;
+
+			if (prev->prev != NULL)
+				prev->prev->next = temp;
+			else
+				*list = temp;
+
+			temp->prev = prev->prev;
+			temp->next = prev;
+			prev->prev = temp;
+			prev->next = next;
+
+			if (next != NULL)
+				next->prev = prev;
+			print_list(*list);
 		}
-		else
-		{
-			listint_t *p = head;
-			while (p != NULL)
-			{
-				if (p->next == NULL || current->n < p->next->n)
-				{
-					current->next = p->next;
-					p->next = current;
-					break;
-				}
-				p = p->next;
-			}
-		}
-		print_list(head);
+		current = next;
 	}
-	*list = head;
 }
